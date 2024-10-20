acd635f9fccd1f5f1a78bb3180abb2d5d3941b66
tanayduddalwar <tanay.duddalwar@gmail.com>
2024-10-20 15:22:29 +0530

    changes

--- DBMSL/Assignment12/Assignment12.java
+++ DBMSL/Assignment12/Assignment12.java
@@ -11,6 +11,9 @@ import org.bson.Document;
 public class Mongo{
 	public static void main(String[] args){
 		MongoCredential client=new MongoClient("127.0.0.1",27017);
+	// 	    MongoCredential credentials = MongoCredential.createCredential("te31123","te31123_db", "te31123".toCharArray());
+    // MongoClient client = new MongoClient(new ServerAddress("10.10.12.248",27017), Arrays.asList(credentials));
+
 		System.out.println("Connected to DB");
 		MongoDatabase db=client.getDatabase("31123_db");
 		MongoCollection<Document>collection=db.getCollection("Employee");
@@ -51,3 +54,43 @@ public class Mongo{
             if (ans.equals("N") || ans.equals("n"))
                 break;
 }
+ public static void createDocument(MongoCollection<Document> collection, Scanner sc) {
+        System.out.println("Enter a key: ");
+        String key = sc.next();
+        System.out.println("Enter its value: ");
+        String value = sc.next();
+        Document newdoc = new Document(key, value);
+        collection.insertOne(newdoc);
+        System.out.println("Document inserted successfully");
+
+    }
+
+    public static void readDocuments(MongoCollection<Document> collection) {
+        for (Document doc : collection.find()) {
+            System.out.println(doc.toJson());
+        }
+    }
+
+    public static void updateDocument(MongoCollection<Document> collection, Scanner sc) {
+        System.out.println("Enter document identifier key: ");
+        String key = sc.next();
+        System.out.println("Enter corresponding value");
+        String value = sc.next();
+        Document filter = new Document(key, value);
+        System.out.println("Enter new value");
+        String uValue = sc.next();
+        Document uDoc = new Document(key, uValue);
+        collection.updateOne(filter, uDoc);
+        System.out.println("Document updated successfully");
+    }
+
+    public static void deleteDocument(MongoCollection<Document> collection, Scanner sc) {
+        System.out.println("Enter document identifier key: ");
+        String key = sc.next();
+        System.out.println("Enter corresponding value");
+        String value = sc.next();
+        Document filter = new Document(key, value);
+        collection.deleteOne(filter);
+        System.out.println("Document deleted successfully");
+
+    }
