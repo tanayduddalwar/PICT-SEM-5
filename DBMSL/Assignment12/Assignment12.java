09b0cf773b2e423219e94180558cfa331d0c684f
Tanay Duddalwar <119003967+tanayduddalwar@users.noreply.github.com>
2024-11-05 23:50:08 +0530

    Update Assignment12.java

--- DBMSL/Assignment12/Assignment12.java
+++ DBMSL/Assignment12/Assignment12.java
@@ -1,96 +1,94 @@
-package v3;
-
-import java.util.*;
-
-import com.mongodb.MongoClient;
-import com.mongodb.client.*;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
+package mongoJava;
+import java.util.Scanner;
 import org.bson.Document;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 
-public class Mongo{
-	public static void main(String[] args){
-		MongoCredential client=new MongoClient("127.0.0.1",27017);
-	// 	    MongoCredential credentials = MongoCredential.createCredential("te31123","te31123_db", "te31123".toCharArray());
-    // MongoClient client = new MongoClient(new ServerAddress("10.10.12.248",27017), Arrays.asList(credentials));
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Connecting to MongoDB...");
+            MongoClient mongo = new MongoClient("localhost", 27017);
+            MongoDatabase db = mongo.getDatabase("mock_prac");
+            if (db.getCollection("Employee") == null) {
+                db.createCollection("Employee");
+            }
+            MongoCollection<Document> collection = db.getCollection("Employee");
+            Scanner sc = new Scanner(System.in);
+            int choice;
+            while (true) {
+                System.out.println("\nChoose an option:");
+                System.out.println("1: Insert");
+                System.out.println("2: Update");
+                System.out.println("3: Delete");
+                System.out.println("4: Display All Documents");
+                System.out.println("5: Exit");
+                choice = sc.nextInt();
+                sc.nextLine(); // Consume newline
 
-		System.out.println("Connected to DB");
-		MongoDatabase db=client.getDatabase("31123_db");
-		MongoCollection<Document>collection=db.getCollection("Employee");
-		Scanner sc=new Scanner(System.in);
-		while(true){
-			System.out.println("Menu:");
-            System.out.println("1. Create");
-            System.out.println("2. Read");
-            System.out.println("3. Update");
-            System.out.println("4. Delete");
-            System.out.println("5. Exit");
-            System.out.print("Enter your choice: ");
-			int choice=sc.nextInt();
-			switch(choice){
-				case 1:
-					createdocument(collection,sc);
-				case 2:
-					readDocuments(collection);
-				case 3:
-                    updateDocument(collection, sc);
-                    break;
+                switch (choice) {
+                    case 1:
+                        System.out.println("Inserting a new document.");
+                        System.out.print("Enter name: ");
+                        String name = sc.nextLine();
+                        System.out.print("Enter department: ");
+                        String dept = sc.nextLine();
+                        System.out.print("Enter location: ");
+                        String loc = sc.nextLine();
+                        System.out.print("Enter salary: ");
+                        int salary = sc.nextInt();
 
-                case 4:
-                    deleteDocument(collection, sc);
-                    break;
+                        Document doc = new Document("name", name)
+                                        .append("dept", dept)
+                                        .append("loc", loc)
+                                        .append("salary", salary);
+                        collection.insertOne(doc);
+                        System.out.println("Document inserted successfully.");
+                        break;
 
-			}
-			System.out.println("Continue? (y/n): ");
-            String ans = sc.next();
-            if (ans.equals("N") || ans.equals("n"))
-                break;
-		}
-		sc.close();
-		client.close();
-	}
-	System.out.println("Continue? (y/n): ");
-            String ans = sc.next();
-            if (ans.equals("N") || ans.equals("n"))
-                break;
-}
- public static void createDocument(MongoCollection<Document> collection, Scanner sc) {
-        System.out.println("Enter a key: ");
-        String key = sc.next();
-        System.out.println("Enter its value: ");
-        String value = sc.next();
-        Document newdoc = new Document(key, value);
-        collection.insertOne(newdoc);
-        System.out.println("Document inserted successfully");
+                    case 2:
+                        System.out.println("Updating a document.");
+                        System.out.print("Enter the name of the employee to update: ");
+                        String nameToUpdate = sc.nextLine();
+                        System.out.print("Enter the new salary: ");
+                        int newSalary = sc.nextInt();
 
-    }
+                        collection.updateOne(Filters.eq("name", nameToUpdate), Updates.set("salary", newSalary));
+                        System.out.println("Document updated successfully.");
+                        break;
 
-    public static void readDocuments(MongoCollection<Document> collection) {
-        for (Document doc : collection.find()) {
-            System.out.println(doc.toJson());
+                    case 3:
+                        System.out.println("Deleting a document.");
+                        System.out.print("Enter the name of the employee to delete: ");
+                        String nameToDelete = sc.nextLine();
+
+                        collection.deleteOne(Filters.eq("name", nameToDelete));
+                        System.out.println("Document deleted successfully.");
+                        break;
+
+                    case 4:
+                        System.out.println("Displaying all documents:");
+                        for (Document document : collection.find()) {
+                            System.out.println(document.toJson());
+                        }
+                        break;
+
+                    case 5:
+                        System.out.println("Exiting...");
+                        mongo.close();
+                        sc.close();
+                        return;
+
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-
-    public static void updateDocument(MongoCollection<Document> collection, Scanner sc) {
-        System.out.println("Enter document identifier key: ");
-        String key = sc.next();
-        System.out.println("Enter corresponding value");
-        String value = sc.next();
-        Document filter = new Document(key, value);
-        System.out.println("Enter new value");
-        String uValue = sc.next();
-        Document uDoc = new Document(key, uValue);
-        collection.updateOne(filter, uDoc);
-        System.out.println("Document updated successfully");
-    }
-
-    public static void deleteDocument(MongoCollection<Document> collection, Scanner sc) {
-        System.out.println("Enter document identifier key: ");
-        String key = sc.next();
-        System.out.println("Enter corresponding value");
-        String value = sc.next();
-        Document filter = new Document(key, value);
-        collection.deleteOne(filter);
-        System.out.println("Document deleted successfully");
-
-    }
+}
